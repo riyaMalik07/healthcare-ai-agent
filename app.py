1c2460c603b912f2ba6e7778b99d6790168bbbd6
Peeyushnaik <peeyushnaik@gmail.com>
2026-02-27 17:22:36 +0530

    Added basic prediction API

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,4 +5,12 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Healthcare AI Agent Started"}
-    
+
+@app.get("/predict")
+def predict():
+    return {
+        "disease": "Common Cold",
+        "urgency": "Low",
+        "medicine": "Paracetamol",
+        "generic": "Acetaminophen"
+    }
