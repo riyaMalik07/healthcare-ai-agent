6203601236ba9bbc14844e1904b18cd445a6e75e
Peeyushnaik <peeyushnaik@gmail.com>
2026-02-27 17:39:16 +0530

    Frontend backend integration working

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allow frontend to connect
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def home():
-    return {"message": "Healthcare AI Agent Started"}
+    return {"message": "Healthcare AI Agent Running"}
+
 
 @app.get("/predict")
-def predict():
-    return {
-        "disease": "Common Cold",
-        "urgency": "Low",
-        "medicine": "Paracetamol",
-        "generic": "Acetaminophen"
-    }
+def predict(symptoms: str):
+
+    symptoms = symptoms.lower()
+
+    if "fever" in symptoms:
+        return {
+            "disease": "Flu",
+            "urgency": "Moderate",
+            "medicine": "Paracetamol"
+        }
+
+    elif "headache" in symptoms:
+        return {
+            "disease": "Migraine",
+            "urgency": "Low",
+            "medicine": "Ibuprofen"
+        }
+
+    else:
+        return {
+            "disease": "Common Cold",
+            "urgency": "Low",
+            "medicine": "Cetirizine"
+        }
